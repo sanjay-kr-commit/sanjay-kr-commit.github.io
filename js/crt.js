79f3f58292fb6512edb2f456c094f45d6a990677
sanjay-kr-commit <sanjay.kr.on@gmail.com>
2026-07-19 22:20:51 +0530

    clean start

--- js/crt.js
+++
@@ -1,8 +0,0 @@
-function glitch() {
-    document.body.style.setProperty('--tear', (10 + Math.random() * 80) + '%');
-    document.body.classList.add('crt-glitch');
-    setTimeout(() => document.body.classList.remove('crt-glitch'), 140);
-}
-(function loop() {
-    setTimeout(() => { glitch(); loop(); }, 8000 + Math.random() * 12000);
-})();
